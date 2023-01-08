93a5ded5cdc51b33435bf15a1d9f9219d1d1437a
jossyShield <jossyboyjoe@gmail.com>
2023-01-08 22:37:40 +0100

    New updates

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         
           <BrowserRouter>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/index" element={<Home />} />
+              <Route path="" element={<Home />} />
+              <Route path="index" element={<Home />} />
               <Route path="about" element={<About />} />
               <Route path="contact" element={<Contact />} />
               <Route path="calendar" element={<Calendar />} />
